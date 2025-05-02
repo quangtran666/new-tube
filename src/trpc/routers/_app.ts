f03ed4058c868631e5a3c059ac6aa5690bbbab36
Apolos666 <quangprox121@gmail.com>
2025-05-02 10:02:14 +0700

    feat: studio videos

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,10 @@
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { createTRPCRouter } from "../init";
+import { studioRouter } from "@/modules/studio/server/procedures";
+import { videosRouter } from "@/modules/video/server/procedure";
 export const appRouter = createTRPCRouter({
+  studio: studioRouter,
+  videos: videosRouter,
   categories: categoriesRouter,
 });
 // export type definition of API
